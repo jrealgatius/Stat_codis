231c2bcea84d07f153dd569e53cb014b73b7deca
jrealgatius <jordireal@gmail.com>
2019-11-07 14:26:00 +0100

    factoritzar() arreglada

--- funcions_propies.R
+++ funcions_propies.R
@@ -394,7 +394,7 @@ factoritzar<-function(dt=dades,variables=c("grup","situacio")) {
   # variables=c("grup","situacio","kk","sexe")
 
   # Només si variable existeix la variable en dt
-  variables<-variables[variables %in% names(dades)]
+  variables<-variables[variables %in% names(dt)]
   
   factoritzacio<-function(x) {if (!is.factor(x)) x<-as.factor(x) else x<-x}
 
