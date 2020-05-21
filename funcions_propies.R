e6ec150c6d48a22b8189df2ae172c0de7714f5a7
jrealgatius <51073451+jrealgatius@users.noreply.github.com>
2020-05-21 15:40:55 +0200

    Afegida funció read_conductor()
    Afegida funció i arguments en extreure.variables() i arguments

--- funcions_propies.R
+++ funcions_propies.R
@@ -62,6 +62,44 @@ directori_treball<-function(subdirectori,directori) {
 
 }
 
+#' Read conductor file different formats txt o rds o xls xlsx o data_frame tibble
+#' @param fitxer Character as a file name and path or data.frame
+#' 
+read_conductor<-function(fitxer,...) {
+  
+  # Si el fitxer es un data_frame saltar
+  if (any(class(fitxer) %in% c("tbl_df","tbl","data.frame"))) 
+    
+    dt <- tibble::as_tibble(fitxer) 
+  
+  else { 
+    
+    if (stringr::str_detect(fitxer,"\\.txt$")){
+      
+      dt<-data.table::fread(fitxer) %>% as_tibble()
+      
+    } else if (stringr::str_detect(fitxer,"\\.rds$")) {
+      
+      dt<-readRDS(fitxer,...) %>% as_tibble()
+      
+    } else if (stringr::str_detect(fitxer,"\\.xls$")) {
+      
+      dt<-readxl::read_excel(fitxer,...) %>% tidyr::as_tibble()
+      
+    } else if (stringr::str_detect(fitxer,"\\.xlsx$")) {
+      
+      dt<-readxl::read_excel(fitxer,...) %>% tidyr::as_tibble()
+      
+    } else if (stringr::str_detect(fitxer,"\\.sav$")) {
+      
+      dt<-foreign::read.spss(fitxer,use.value.labels = T,to.data.frame = T,...) %>% tidyr::as_tibble()
+    } 
+    else {stop("Data format no reconegut ")}
+  }
+  
+}
+
+
 # Actualitza conductor amb noves variables en dades 
 
 ActualitzarConductor<-function(d=dades,taulavariables="VARIABLES_R3b.xlsx") {
@@ -535,7 +573,8 @@ extreure.variables=function(taula="table1",taulavariables="variables_R.xls",vari
   # variable_camp="camp"
   
   ####  Llegir etiquetes i variables a analitzar ####
-  variables <- readxl::read_excel(taulavariables) %>% tidyr::as_tibble() %>% dplyr::select(!!variable_camp,!!taula)
+  # variables <- readxl::read_excel(taulavariables) %>% tidyr::as_tibble() %>% dplyr::select(!!variable_camp,!!taula)
+  variables <- read_conductor(taulavariables,...) %>% dplyr::select(!!variable_camp,!!taula)
   taula_sym<-rlang::sym(taula)
   variables<-variables %>% dplyr::filter(!is.na(!!taula_sym))
   # variables[is.na(variables)]<- 0
