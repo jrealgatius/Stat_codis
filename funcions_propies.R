24effddbb248593112d69ea94066f6a5099b71c6
jrealgatius <jordireal@gmail.com>
2020-07-04 14:47:21 +0200

    Arreglada etiquetar() Afegida generar formula_table1()

--- funcions_propies.R
+++ funcions_propies.R
@@ -325,9 +325,9 @@ llistaNomenada <- function(...) {
 ###
 etiquetar<-function(d=dadestotal,taulavariables="variables_R.xls",camp_descripcio="descripcio") {
   
-  # d=dades
-  # taulavariables = conductor_variables
-  # camp_descripcio="etiqueta2"
+  d=iris
+  taulavariables = etiquetes_iris
+  camp_descripcio="descripcio"
 
   #  Llegir etiquetes i variables a analitzar ####
   variables<-read_conductor(taulavariables)
@@ -338,10 +338,10 @@ etiquetar<-function(d=dadestotal,taulavariables="variables_R.xls",camp_descripci
   
   variables<-variables %>% dplyr::select(camp,descripcio=!!camp_descripcio) 
   
-  # Els que no tenen etiquet assignar el mateix nom del camp
+  # Els que no tenen etiqueta assignar el mateix nom del camp
+  variables<-variables %>% mutate(descripcio=as.character(descripcio))
   variables<-variables %>% mutate(descripcio=ifelse(descripcio=="0" | is.na(descripcio),camp,descripcio)) 
   
-  #
   # Etiquetar variables         
   seleccio<-variables
   camp<- as.vector(seleccio$camp) #
@@ -1045,6 +1045,52 @@ formula_vector<-function(vector=c("sex","age"),y="y",logit=F,eliminar=NA){
   formula
              
 }
+
+
+# Retorna formula per table1::table1 segons llista de varibles
+# Columna de variables amb que vull generar la formula 
+
+# x= variables / y = grups (opcional) / eliminar /  a = Avaluar (primera posició no inclosa en condutor)
+
+formula_table1=function(x="taula1",y="",eliminar=c("IDP"), a="",taulavariables='variables.xls',dt=NA,...) {
+  
+  # x="taula_basal"
+  # y="Species"
+  # eliminar=c("IDP")
+  # a=""
+  # taulavariables=etiquetes_iris
+  # dt=NA
+  
+  variables <- read_conductor(taulavariables,...) %>% data.frame()
+  variables <- read_conductor(taulavariables) %>% data.frame()
+  
+  # variables[is.na(variables)]<- 0
+  x_sym<-rlang::sym(x)
+  variables<-variables %>% dplyr::filter(!is.na(!!x_sym))
+  
+  variables<-variables %>% 
+    dplyr::filter(!!x_sym>0) %>% 
+    dplyr::arrange(!!x_sym)
+  
+  # Verificar si dades estan en conductor
+  if (is.data.frame(dt)) {
+    vars_not_dt<-variables %>% anti_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value"))
+    variables<-variables %>% semi_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value"))
+    warning(paste0("Variables not in data ",vars_not_dt["camp"], ". So, it is not included in formula"))}
+  
+  pepito<-paste("as.vector(variables[variables$",x,">0,]$camp)[!as.vector(variables[variables$",x,">0,]$camp)%in%eliminar]",sep="")
+  
+  llistataula<-eval(parse(text=pepito))
+  if (a!="") llistataula<-c(a,llistataula,a)
+  
+  formu<-paste("",paste(llistataula, collapse=" + "), sep=" ~ ") 
+  
+  if (y!="") formu<-paste0(formu," | ",y)
+  
+  as.formula(formu)
+}
+
+
 
 #  OR.ajustats(x,ajust,y)         ###########
 #
