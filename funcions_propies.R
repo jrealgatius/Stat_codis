10efdedf9881a4f47c1a2db8ef80c849402b8c49
jrealgatius <jordireal@gmail.com>
2020-08-22 11:25:11 +0200

    Afegit parametre extreure.variables() per eliminar variables que no estan en dades

--- funcions_propies.R
+++ funcions_propies.R
@@ -575,19 +575,26 @@ selectorvariables=function(taula="table1",taulavariables="variables_R.xls",dt=da
 
 #  Extreure.Variables: Selector de variables TAULA DE--------
 #
-extreure.variables=function(taula="table1",taulavariables="variables_R.xls",variable_camp="camp",...) {
+extreure.variables=function(taula="table1",taulavariables="variables_R.xls",variable_camp="camp",dt=NA,...) {
   
-  # taula="matching"
-  # taulavariables = conductor
+  # taula="dates_excel"
+  # taulavariables = conductor_variables
   # variable_camp="camp"
+  # dt=dades
   
   ####  Llegir etiquetes i variables a analitzar ####
   # variables <- readxl::read_excel(taulavariables) %>% tidyr::as_tibble() %>% dplyr::select(!!variable_camp,!!taula)
   variables <- read_conductor(taulavariables,...) %>% dplyr::select(!!variable_camp,!!taula)
   taula_sym<-rlang::sym(taula)
   variables<-variables %>% dplyr::filter(!is.na(!!taula_sym))
-  # variables[is.na(variables)]<- 0
-
+  
+  # Verificar si columnes del conductor estan en dt
+  if (is.data.frame(dt)) {
+    vars_not_dt<-variables %>% anti_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value")) %>% pull("camp")
+    variables<-variables %>% semi_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value"))
+    paste0("Variables not in data: ",paste0(vars_not_dt,collapse = ", "), ". So, it is not included in formula") %>% warning()
+    }
+  
   # filtratge 
   kk<-variables %>% dplyr::arrange(!!taula_sym) %>% dplyr::select(!!variable_camp) %>% as.vector()
   kk<-as.vector(kk[[1]])
@@ -1090,9 +1097,10 @@ formula.text=function(x="taula1",y="resposta",eliminar=c("IDP"), a="",taulavaria
   
   # Verificar si dades estan en conductor
   if (is.data.frame(dt)) {
-    vars_not_dt<-variables %>% anti_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value"))
+    vars_not_dt<-variables %>% anti_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value")) %>% pull("camp")
     variables<-variables %>% semi_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value"))
-    warning(paste0("Variables not in data ",vars_not_dt["camp"], ". So, it is not included in formula"))}
+    paste0("Variables not in data: ",paste0(vars_not_dt,collapse = ", "), ". So, it is not included in formula") %>% warning()
+   }
   
   pepito<-paste("as.vector(variables[variables$",x,">0,]$camp)[!as.vector(variables[variables$",x,">0,]$camp)%in%eliminar]",sep="")
   
@@ -1143,9 +1151,10 @@ formula_table1=function(x="taula1",y="",eliminar=c("IDP"), a="",taulavariables='
   
   # Verificar si dades estan en conductor
   if (is.data.frame(dt)) {
-    vars_not_dt<-variables %>% anti_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value"))
+    vars_not_dt<-variables %>% anti_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value")) %>% pull("camp")
     variables<-variables %>% semi_join(names(dt) %>% as_tibble(camp=value),by=c("camp"="value"))
-    warning(paste0("Variables not in data ",vars_not_dt["camp"], ". So, it is not included in formula"))}
+    paste0("Variables not in data: ",paste0(vars_not_dt,collapse = ", "), ". So, it is not included in formula") %>% warning()
+  }
   
   pepito<-paste("as.vector(variables[variables$",x,">0,]$camp)[!as.vector(variables[variables$",x,">0,]$camp)%in%eliminar]",sep="")
   
