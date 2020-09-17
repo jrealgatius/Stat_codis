882c1236895c9aa11cb04e151cc866888dc3e2d9
jrealgatius <jordireal@gmail.com>
2020-09-17 12:06:21 +0200

    Afegir casos en netejar_noms_variables()

--- funcions_propies.R
+++ funcions_propies.R
@@ -5362,14 +5362,14 @@ netejar.noms.variables<-function(dt=LIPOS_EORTEGA){
     stringr::str_trim() %>% 
     stringr::str_replace_all(" ","_") %>% 
     stringr::str_replace_all("-","_") %>%
-    stringr::str_replace_all("([.])\\1+","\\1")
-    
+    stringr::str_replace_all("([.])\\1+","\\1") %>% 
+    stringr::str_replace_all("\\*","") %>% 
+    stringr::str_replace_all("\\?","")
+  
   names(dt)<-paco
   dt
   
 }
-
-
 
 
 # Funció que elimina accents dels noms de les variables
