3a06820eb45d9fab0ecd1e34fda5adca8d41c821
jrealgatius <jordireal@gmail.com>
2021-02-24 11:26:36 +0100

    Millorada la funció Pvalors_ajustats_Update_Compare()

--- funcions_propies.R
+++ funcions_propies.R
@@ -2982,20 +2982,27 @@ Pvalors_ajustats_compare<-function(objecte_compare=T1.1.2, metodo="BH",p="p.over
 }
 
 ## Actualitzar p-valors d'un objecte CompareGroups
-Pvalors_ajustats_Update_Compare<-function(objecte_compare=res,p="p.overall",metodo="BH") {
+Pvalors_ajustats_Update_Compare<-function(objecte_compare=res,p="p.overall",method ="BH") {
+  
+  # objecte_compare=res
+  # p="p.trend"
+  # method="bonferroni"
+  
   # 1. Extrect els p-valors 
   pvalors <- compareGroups::getResults(objecte_compare, p)
   
   # 2. Recalculo p-valors segons metode
-  pvalors<-stats::p.adjust(pvalors, method = metodo) 
+  pvalors<-stats::p.adjust(pvalors, method = method) 
   
   # 3. Actualitzo Objecte compare
   vars_names<-names(objecte_compare)
+
   for(i in unique(vars_names)){                                # achieved here
-    objecte_compare[[i]]["p.overall"]$p.overall <- pvalors[i] }
+    # objecte_compare[[i]][p]$p.overall <- pvalors[i] 
+    objecte_compare[[i]][p][p]<- pvalors[i] }
   # 4. Resultat
   objecte_compare
-}
+  }
 
 Pvalors_ajustats_taula<-function(objecte_taula=OR.ajust, p.valors='p valor', metodo="BH") {
   
