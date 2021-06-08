bc85fc969ace6560d99881c358db076a419b74e8
jrealgatius <jordireal@gmail.com>
2021-06-08 23:44:22 +0200

    Afegit en agregar_problemes() argument cataleg_mana =T/F per dir-li que generi tants agregadors com inclou cataleg, encara que estiguin en blanc.

--- funcions_propies.R
+++ funcions_propies.R
@@ -3218,22 +3218,22 @@ agregar_analitiques<-function(dt=ANALITIQUES,bd.dindex="20161231",finestra.dies=
 ###################     LLANCO PROBLEMES LONG + UNA DATA INDEX / BD ab data index, finestra temporal -> 
 #                       RETORNO UNA TAULA AGREGADA / TAMBÉ POSO LA TAULA CATALEG
 
-agregar_problemes<-function(dt=PROBLEMES,bd.dindex="20161231",dt.agregadors=CATALEG,finestra.dies=c(-Inf,0),prefix="DG.",camp_agregador="agr",keep.code=F) {
+agregar_problemes<-function(dt=PROBLEMES,bd.dindex="20161231",dt.agregadors=CATALEG,finestra.dies=c(-Inf,0),prefix="DG.",camp_agregador="agr",keep.code=F,cataleg_mana=F) {
+
 
   # dt=dt_problemes
-  # bd.dindex =dt_dataindex
-  # dt.agregadors=conductor_cataleg
-  # finestra.dies=c(0,+Inf)
+  # bd.dindex ="20191231"
+  # dt.agregadors=dt_cataleg
+  # finestra.dies=c(-Inf,0)
   # prefix = "DG."
   # camp_agregador = "agr"
-  # keep.code=T
-  
-  
+  # keep.code=F
+  # cataleg_mana=T
+
   ## afegir en dataindex de BDINDEX si bd.dindex<>""
   #### Afegir + data index (+dtindex) en l'historic de problemes
   
   dt<-afegir_dataindex(dt,bd.dindex)
-  
   
   ## filtrar per intervals de dates 
 
@@ -3262,24 +3262,39 @@ agregar_problemes<-function(dt=PROBLEMES,bd.dindex="20161231",dt.agregadors=CATA
     dplyr::select(cod,agr=!!camp_agregador_sym) %>% 
     filter(!is.na(agr))
 
-  ## Capturar agregador 
-  dt.temp<-dt %>% 
+  ## Captura d'agregadors     ######
+  dt<-dt %>% 
     # camps mínims que necessito per agregar 
     dplyr::select(c(idp,dtindex,cod,dat)) %>%                                             # Selecciono camps mínims
     # Capturo Agregador de CATALEG
-    dplyr::inner_join(dplyr::select(dt.agregadors,c(cod,agr)), by="cod") %>%      # Capturo agregador del cataleg
-    # Eliminar duplicats agafant el primer registre (dat minima)
+    dplyr::inner_join(dplyr::select(dt.agregadors,c(cod,agr)), by="cod")                  # Capturo agregador del cataleg
+
+  ## Formatejo 
+  dt.temp<-dt %>% 
+    # Eliminar duplicats agafant el primer registre (dat mes antiga --> minima)
     # Agrupar= unic reg per idp-agr (mes antic segons data)
-    dplyr::group_by(idp,dtindex,agr) %>%                                                  # Agrupo per idp agr
+    dplyr::group_by(idp,dtindex,agr) %>%                                          # Agrupo per idp agr
     dplyr::slice(which.min(dat)) %>%                                              # Selecciono més antic 
     dplyr::ungroup() # desagrupo
+ 
+  # Si s'han d'incloure els agregadors sense codi en base de dades s'ha d'ampliar dt (dt.temp) i afegir cod null sese dat
+  
+  if (cataleg_mana) {
+    # Selecciono agregadors en cataleg sense codi en dt
+    # tots els codis que tenen algun agregador en dt i els que no
+    dt_temp2<-dplyr::select(dt,cod) %>% distinct(cod) %>% left_join(dplyr::select(dt.agregadors,c(cod,agr)),by="cod")
+    pp<-dplyr::select(dt.agregadors,agr) %>% distinct() %>% anti_join(dt_temp2 %>% distinct(agr),by="agr")
+    porca<-dt.temp %>% distinct(idp,dtindex) %>% merge(pp) %>% as_tibble() %>% mutate(dat=NA)
+    # Afegeixo en dt.temp els nous agregadors buits i fusiono amb dt.temp
+    dt.temp<-dt.temp %>% bind_rows(porca)
+    }
   
     # RESHAPE una data per agregador  
     # seleccionar camps i Reshape  
-    dt.agregat<-dt.temp %>% 
+  dt.agregat<-dt.temp %>% 
       dplyr::select(idp,agr,dat,dtindex) %>%  # Selecciono agregador i data
     # RESHAPE per agregador i em quedo la data
-      tidyr::spread(agr,dat,sep=".")                                                        # Reshape
+      tidyr::spread(agr,dat,sep=".")    # Reshape
   
   names(dt.agregat) <- sub("agr.", prefix, names(dt.agregat))   # Afegir prefix en noms de variables 
 
@@ -3293,7 +3308,7 @@ agregar_problemes<-function(dt=PROBLEMES,bd.dindex="20161231",dt.agregadors=CATA
   dt.agregat<-dt.agregat %>% left_join(dt.agregat_cod,by=c("idp","dtindex"))
   }
   
-  dt.agregat
+  dt.agregat 
   
 }
 
